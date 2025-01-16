4c20a520de309d0ad67bf8204af105549b07a908
BIKASH NAIK <104919329+Bikashnaik07@users.noreply.github.com>
2025-01-16 20:52:24 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+        numb: 1,
+        question:" Which classical dance form originated in the state of Kerala?",
+        answer:"D) Kathakali",
+        options:[
+           " A. Bharatanatyam",
+            "B. Kathak",
+            "C. Odissi",
+            "D. Kathakali"
+
+        ]
+    },
+    {
+        numb: 2,
+        question:"Which of the following is a UNESCO World Heritage Site in India?",
+        answer:"D) All of the above",
+        options:[
+            "A. Taj Mahal",
+            "B. Qutub Minar",
+            "C. Hampi",
+            "D. All of the above"
+        ]
+    },
+    {
+        numb: 3,
+        question:"Which festival is famously associated with the state of Punjab?",
+        answer:"B) Baisakhi",
+        options:[
+            "A) Pongal",
+            "B) Baisakhi",
+            "C) Onam",
+            "D) Durga Puja"
+        ]
+    },
+    {
+        numb: 4,
+        question:"In which Indian state is the Sun Temple at Konark located?",
+        answer:"A) Odisha",
+        options:[
+            "A) Odisha",
+            "B) Gujarat",
+            "C) Rajasthan", 
+            "D) Tamil Nadu"
+        ]
+    },
+    {
+        numb: 5,
+        question:"Which Indian musical instrument is a wind instrument?",
+        answer:" C) Flute",
+        options:[
+            "A) Tabla",
+            "B) Sitar",
+            "C) Flute",
+            "D) Mridangam"
+        ]
+    },
+];
